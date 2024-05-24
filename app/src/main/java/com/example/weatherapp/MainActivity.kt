e34f8634172b2b20e36b53a7c87769b24d3538b9
bone204 <truongbmt4@gmail.com>
2024-05-24 08:35:19 +0700

    sửa bottom navigation

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.weatherapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Cloud
 import androidx.compose.material.icons.filled.MusicNote
+import androidx.compose.material.icons.filled.Newspaper
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
@@ -21,9 +28,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
@@ -31,7 +41,10 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.weatherapp.ui.advancedscreen.Advanced
 import com.example.weatherapp.ui.homescreen.Home
+import com.example.weatherapp.ui.theme.Black20
+import com.example.weatherapp.ui.theme.Blue20
 import com.example.weatherapp.ui.theme.GreenCL
+import com.example.weatherapp.ui.theme.Grey60
 import com.example.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -61,13 +74,18 @@ fun BottomNavigation() {
     Scaffold(
         bottomBar = {
             BottomAppBar(
-                containerColor = Color.Transparent,
+                containerColor = Black20,
                 tonalElevation = 8.dp,
             ) {
                 BottomNavigationItem(
-                    icon = { Icon(Icons.Default.Cloud, contentDescription = null,
-                        tint = if(selected.value == Icons.Default.Cloud) Color.White else Color.DarkGray ) },
-                    label = { Text("Cloud", color = if(selected.value == Icons.Default.Cloud) Color.White else Color.DarkGray) },
+                    icon = {
+                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                            Icon(Icons.Default.Cloud, contentDescription = null,
+                                tint = if(selected.value == Icons.Default.Cloud) Blue20 else Grey60)
+                            Spacer(modifier = Modifier.height(4.dp)) // Add space between icon and label
+                            Text("Weather", fontWeight = FontWeight.Bold, color = if(selected.value == Icons.Default.Cloud) Blue20 else Grey60)
+                        }
+                    },
                     selected = selected.value == Icons.Default.Cloud,
                     onClick = {
                         selected.value = Icons.Default.Cloud
@@ -78,12 +96,17 @@ fun BottomNavigation() {
                     modifier = Modifier.weight(1f)
                 )
                 BottomNavigationItem(
-                    icon = { Icon(Icons.Default.Settings, contentDescription = null,
-                        tint = if(selected.value == Icons.Default.Settings) Color.White else Color.DarkGray) },
-                    label = { Text("Advanced", color = if(selected.value == Icons.Default.Settings) Color.White else Color.DarkGray) },
-                    selected = selected.value == Icons.Default.Settings,
+                    icon = {
+                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                            Icon(Icons.Default.Newspaper, contentDescription = null,
+                                tint = if(selected.value == Icons.Default.Newspaper) Blue20 else Grey60)
+                            Spacer(modifier = Modifier.height(4.dp)) // Add space between icon and label
+                            Text("News", fontWeight = FontWeight.Bold, color = if(selected.value == Icons.Default.Newspaper) Blue20 else Grey60)
+                        }
+                    },
+                    selected = selected.value == Icons.Default.Newspaper,
                     onClick = {
-                        selected.value = Icons.Default.Settings
+                        selected.value = Icons.Default.Newspaper
                         navigationController.navigate(Screen.Advanced.screen){
                             popUpTo(0)
                         }
--- app/src/main/java/com/example/weatherapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/weatherapp/ui/theme/Color.kt
@@ -14,6 +14,7 @@ val GreenCL = Color(0xFF3FDC85)
 val Green40 = Color(0xFF6B9B42)
 
 val Grey40 = Color(0xFFE9E9E9)
+val Grey60 = Color(0xFF757575)
 val Grey80 = Color(0xFF444444)
 
 val NavyBlue = Color(0xFF08244F)
@@ -27,6 +28,9 @@ val LightSky = Color(0xFF2DC8EA)
 val Sky60 = Color(0xFF319CCC)
 val Sky80 = Color(0xFF268BB9)
 
+val Blue20 = Color(0xFF1A73E8)
 val Blue40 = Color(0xFF91DEFF)
 val Blue60 = Color(0xFF4B337B)
 val Blue80 = Color(0xFF2F226B)
+
+val Black20 = Color(0xFF161616)
